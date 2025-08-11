c617ea6a0d2af9f9e3e3f107a76cacf984076ffa
Pine Hein <116600234+pinehein2@users.noreply.github.com>
2025-08-11 17:19:21 +0800

    Uploaded fabric mod template

---
+++ src/main/java/tmcm/simpletranslator/SimpleTranslatorDataGenerator.java
@@ -0,0 +1,11 @@
+package tmcm.simpletranslator;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SimpleTranslatorDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
